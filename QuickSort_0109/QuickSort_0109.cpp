1479ff191a877e0485b02c1d0ac0a62dc25e91c1
ichool <faisalaja2653@gmail.com>
2024-03-29 11:43:44 +0700

    int main

--- QuickSort_0109/QuickSort_0109.cpp
+++ QuickSort_0109/QuickSort_0109.cpp
@@ -103,5 +103,22 @@ void display() {
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char ch;
+
+    do {
+
+        input();
+        q_short(0, n - 1);
+        display();
+        cout << "\n\nDo you want to continue? (y/n): ";
+        cin >> ch;
+        if (ch == 'n' || ch == 'N')
+            break;
+
+        system("pause");
+        system("cls");
+
+    } while (true);
+
+    return 0;
 }
