629c0b7c8200aefdba58b832885aaa95acc359f1
ichool <faisalaja2653@gmail.com>
2024-03-29 11:07:35 +0700

    void swap

--- QuickSort_0109/QuickSort_0109.cpp
+++ QuickSort_0109/QuickSort_0109.cpp
@@ -32,6 +32,18 @@ void input() {
     }
 }
 
+//swaps the element at index x with the element at index y 
+void swap(int x, int y)
+{
+    int temp = arr[x];
+
+    arr[x] = arr[y];
+
+    arr[y] = temp;
+
+    mov_count++;
+}
+    
 
 int main()
 {
