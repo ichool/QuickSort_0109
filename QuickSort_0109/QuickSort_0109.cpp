7866b2d2076807ef14e0cf9a6231cc501f9d0b1b
ichool <faisalaja2653@gmail.com>
2024-03-29 10:54:45 +0700

    Add project files.

---
+++ QuickSort_0109/QuickSort_0109.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;
+
+//array of integers to hold values
+
+int arr[20];
+int cmp_count = 0; // number of comparasion
+int mov_count = 0; // number of data movements
+int n;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
