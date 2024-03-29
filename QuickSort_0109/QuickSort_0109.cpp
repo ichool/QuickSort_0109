f5f6985bf754483803f9f2cd95be8952a5f185ec
ichool <faisalaja2653@gmail.com>
2024-03-29 11:04:24 +0700

    input 2

--- QuickSort_0109/QuickSort_0109.cpp
+++ QuickSort_0109/QuickSort_0109.cpp
@@ -21,6 +21,16 @@ void input() {
             cout << "\nMaksimum panjang array adalah 20" << endl;
     }
 
+    cout << "\n-------------------" << endl;
+    cout << "\nEnter Array Element" << endl;
+    cout << "\n-------------------" << endl;
+
+    for (int i = 0; 1 < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
+}
 
 
 int main()
